5f356fe9c53e9516b591b233e95af2a014bddaea
Dan Vega <danvega@gmail.com>
2016-10-22 15:55:47 -0400

    Added core demo

---
+++ core/bookstore/src/main/webapp/app/admin/admin.state.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('bookstoreApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig ($stateProvider) {
+        $stateProvider.state('admin', {
+            abstract: true,
+            parent: 'app'
+        });
+    }
+})();
